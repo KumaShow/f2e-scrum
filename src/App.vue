41541ee2adf625ea3158fdc2a25cc94f7c83c5ec
KumaShow <kumadth@gmail.com>
2023-08-28 22:20:31 +0800

    新增 Copyright

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,9 @@ import ProgressBar from './components/ProgressBar.vue';
 <template>
   <ProgressBar />
   <RouterView />
+  <footer class="text-primary text-center text-sm py-3 bg-bg-dark
+    fixed bottom-0 w-full"
+  >
+    © 2022 The F2E | UI Design - EG | F2E - Benson
+  </footer>
 </template>

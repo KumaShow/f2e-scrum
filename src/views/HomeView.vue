4ced6982f7de38843a1c19201ee4d52d61f38cf1
KumaShow <kumadth@gmail.com>
2023-08-28 22:21:44 +0800

    新增首頁按鈕
    調整樣式並新增按鈕

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,23 @@
         alt="Logo"
         src="../assets/images/logo_hole.png"
       >
-      <h2 class="absolute bottom-[15%] left-1/2 transform -translate-x-1/2">深入敏捷の村一探究竟</h2>
+      <div class="absolute bottom-[15%] left-1/2 transform -translate-x-1/2 text-center">
+        <h2 class="mb-4">
+          深入敏捷の村一探究竟
+        </h2>
+        <div class="font-bold rounded-[40px] bg-primary  px-12 py-3
+        tracking-[0.2rem] inline-block relative"
+        >
+          <button class="font-bold rounded-[40px] bg-primary-dark px-12 py-3
+            bg-gradient-to-b from-primary to-primary-dark tracking-[0.2rem]
+            absolute left-0 top-[-5px] opacity-80 transition ease-out backdrop-[5px]
+            hover:translate-y-[3px] hover:transition-all hover:duration-300"
+          >
+            進入村莊
+          </button>
+          進入村莊
+        </div>
+      </div>
     </div>
   </main>
 </template>
